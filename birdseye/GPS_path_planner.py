03daa7fe9669cf0d1c4c789959b54a59d5137cc3
nubby saurus <nubby@mail.com>
2024-07-16 13:56:20 -0700

    added number of sides and radius params

--- birdseye/GPS_path_planner.py
+++ birdseye/GPS_path_planner.py
@@ -58,12 +58,8 @@ def build_dji_plan(
         w_sides         (int)   :
 
     Todo:
-        * Add whifferdill options, including:
-            + # of edges
-            + radius
-            + enabled/disabled
-        * Add option for enabling/disabling TSP.
-        * Add option for enabling/disabling plotting.
+        * Integrate args into the below.
+        * Cleanup.
     """
     # NOTE: Set data path here.
     #fp_in = "catch/data.csv"
@@ -74,10 +70,12 @@ def build_dji_plan(
     clicks_csv = fp_in
     plan_kml = fp_out
     
-    # thetas = [i*60*np.pi/180 for i in range(6)]
-    thetas = [i*90*np.pi/180 for i in range(4)]
-    hex = lambda pt: [[pt[0] + np.cos(th), pt[1] + np.sin(th)] for th in thetas]
-    square = lambda pt: [[pt[0] + np.cos(th), pt[1] + np.sin(th)] for th in thetas]
+    # Generate polygons around each point provided.
+    thetas = lambda n: [((i * 2 * np.pi) / n) for i in range(n)]
+    poly = lambda pt, r, n: [
+        [pt[0] + r * np.cos(th),
+         pt[1] + r * np.sin(th)] for th in thetas(n)
+    ]
 
     LLrep = []
     UTMrep = []
@@ -103,7 +101,7 @@ def build_dji_plan(
     dbscan = DBSCAN(eps=EPS, min_samples=MIN_SAMPLES).fit(UTMrep)  # cluster in the UTM/cartesian representation
     labels = dbscan.labels_
     print('labels: ', labels)
-# Number of clusters in labels, ignoring noise if present.
+    # Number of clusters in labels, ignoring noise if present.
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_noise_ = list(labels).count(-1)
 
@@ -114,7 +112,7 @@ def build_dji_plan(
     unique_labels = set(labels)
     core_samples_mask = np.zeros_like(labels, dtype=bool)
     core_samples_mask[dbscan.core_sample_indices_] = True
-# print('core_samples_mask: ', core_samples_mask)
+    # print('core_samples_mask: ', core_samples_mask)
 
     colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
     waypoints = []
@@ -140,9 +138,9 @@ def build_dji_plan(
         )
         for pt in xy:
             if skip_convhull:
-                waypoints += square(pt)
+                waypoints += poly(pt, w_rad, w_sides)
             else:
-                tmp += square(pt)
+                tmp += poly(pt, w_rad, w_sides)
 
         xy = UTMrep[class_member_mask & ~core_samples_mask]
         plt.plot(
@@ -155,10 +153,12 @@ def build_dji_plan(
         )
         for pt in xy:
             if skip_convhull:
-                waypoints += square(pt)
+                waypoints += poly(pt, w_rad, w_sides)
             else:
-                tmp += square(pt)
+                tmp += poly(pt, w_rad, w_sides)
+
         tmp = np.array(tmp)
+
         if len(tmp) != 0:
             hull = ConvexHull(tmp)
             pts = tmp[hull.vertices]
@@ -167,7 +167,6 @@ def build_dji_plan(
 
     waypoints = np.array(waypoints)
     plt.title(f"Estimated number of clusters: {n_clusters_}")
-# plt.show()
     for i, pti in enumerate(waypoints):
         tmp = []
         for j, ptj in enumerate(waypoints[i+1:]):
@@ -176,8 +175,7 @@ def build_dji_plan(
 
     plan = []
     places = [i for i in range(len(waypoints))]
-# print(places)
-    out = tsp(places, dists)
+    out = tsp(places, dists) if do_tsp else places
     for pt in waypoints[out]:
         plan.append(list(utm.to_latlon(pt[0], pt[1], u[-2], u[-1])))
     plan = np.array(plan)
