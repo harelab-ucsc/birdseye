35b89e3f9f9ab51671b9b4b9003edb92ecae566f
Jake Lee <jlee211@ucsc.edu>
2025-01-23 16:24:17 -0800

    checkpoint: Removes a try-finally that is undocumented

--- birdseye/GPS_path_planner.py
+++ birdseye/GPS_path_planner.py
@@ -129,9 +129,8 @@ def preprocessWaypoints(waypoints, min_gap=DJI_MIN_DISTANCE):
     #dists *= 1000  # convert to mm from m
     dists *= 100    # convert to cm from m
     dists = dists.astype(np.int32)
-    # plt.imshow(dists)
-    # plt.show()
 
+<<<<<<< Updated upstream
     while dists[np.nonzero(dists)].min() < min_gap:
         d_tmp = np.nonzero(dists)
         indx = np.where(dists == dists[d_tmp].min())
